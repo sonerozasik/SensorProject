15f89bafabbad1ffcf0b76e3c708e8afe4c2645b
Kutay Akgün <seref.kutay.akgun@gmail.com>
2022-07-24 18:23:20 +0300

    login page

---
+++ Mobile/sensor_mobile/lib/constants.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const koyumavi = Color(0xFF22577E);
+const acikmavi = Color(0xFF5584AC);
+const suyesilimsi = Color(0xFF95D1CC);
+const beige = Color(0xFFF6F2D4);
+
+final kHintTextStyle = TextStyle(
+  color: Colors.white54,
+  fontFamily: 'OpenSans',
+);
+
+final kLabelStyle = TextStyle(
+  color: Colors.white,
+  fontWeight: FontWeight.bold,
+  fontFamily: 'OpenSans',
+);
+
+final kBoxDecorationStyle = BoxDecoration(
+  color: Color(0xFF5584AC),
+  borderRadius: BorderRadius.circular(10.0),
+  boxShadow: [
+    BoxShadow(
+      color: Colors.black12,
+      blurRadius: 6.0,
+      offset: Offset(0, 2),
+    ),
+  ],
+);
