1280a8c0272f26a64329cb15f6e389fcca19c26f
Kübranur YEĞİNER <yeginerkubranur@gmail.com>
2022-07-25 14:17:12 +0300

    webUı dosyası yüklendi.

---
+++ WebUI/sensor_webUI/src/components/DashboardForm.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+       
+        </div>
+</template>
+
+
+<script>
+export default{
+    data(){
+        return {
+            
+        }
+    },
+    methods: {
+        
+
+    }
+}
+</script>
+
+
+<style>
+
+</style>
