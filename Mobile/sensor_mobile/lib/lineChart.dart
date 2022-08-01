29ba3ec903c344bc53ea781c6e8a51811e13e416
Kutay Akgün <seref.kutay.akgun@gmail.com>
2022-08-01 17:22:11 +0300

    error fix

--- Mobile/sensor_mobile/lib/lineChart.dart
+++ Mobile/sensor_mobile/lib/lineChart.dart
@@ -1,26 +1,94 @@
+import 'dart:async';
+
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 import 'package:sensor_mobile/dashboard_grid.dart';
+import 'package:sensor_mobile/models/post.dart';
+import 'package:sensor_mobile/services/remote_service.dart';
 
-class LineChartView extends StatelessWidget {
-  LineChartView({Key? key}) : super(key: key);
+class LineChartView extends StatefulWidget {
+  const LineChartView({Key? key}) : super(key: key);
+
+  @override
+  _LineChartViewState createState() => _LineChartViewState();
+}
+
+class _LineChartViewState extends State<LineChartView> {
+  List<FlSpot>? spots = [FlSpot(0.0, 0.0)];
+  List<Post>? posts;
+  Timer? timer;
+  var len = 0;
+
+  double minvalue = 3000;
+  double maxvalue = -5;
+  void initState() {
+    super.initState();
+    // timer = Timer.periodic(const Duration(seconds: 1), (Timer t) => getData());
+    getData();
+  }
+
+  getData() async {
+    posts = await RemoteService().getPosts();
+    if (posts != null) {
+      setState(() {
+        len = posts!.length;
+        spots!.clear();
+        for (int i = 0; i < 50; i++) {
+          spots!.add(FlSpot(i.toDouble(), posts![len - 50 + i].pil.toDouble()));
+          if (posts![len - i - 1].pil.toDouble() > maxvalue) {
+            maxvalue = posts![len - i - 1].pil.toDouble();
+          }
+          if (posts![len - i - 1].pil.toDouble() < minvalue) {
+            minvalue = posts![len - i - 1].pil.toDouble();
+          }
+        }
+      });
+    }
+  }
 
   Widget bottomTitleWidgets(double value, TitleMeta meta) {
     const style = TextStyle(
       fontSize: 10,
-      color: Colors.purple,
+      color: Color.fromARGB(255, 31, 52, 97),
       fontWeight: FontWeight.bold,
     );
-
+    if (value < 0 || value > 49) return Container();
     return SideTitleWidget(
       axisSide: meta.axisSide,
-      space: 5,
-      child: Text(value.toString(), style: style),
+      space: 4,
+      child: (posts != null)
+          ? Text(
+              posts![(len - 50 + value).toInt()].date.hour.toString() +
+                  ":" +
+                  posts![(len - 50 + value).toInt()].date.minute.toString() +
+                  ":" +
+                  posts![(len - 50 + value).toInt()].date.second.toString() +
+                  "\n" +
+                  posts![(len - 50 + value).toInt()].date.day.toString() +
+                  "." +
+                  posts![(len - 50 + value).toInt()].date.month.toString() +
+                  "." +
+                  posts![(len - 50 + value).toInt()].date.year.toString(),
+              textAlign: TextAlign.center,
+              style: style,
+            )
+          : const SizedBox(
+              child: CircularProgressIndicator(
+                strokeWidth: 0.5,
+              ),
+              height: 10.0,
+              width: 10.0,
+            ),
     );
   }
 
   Widget leftTitleWidgets(double value, TitleMeta meta) {
-    const style = TextStyle(fontSize: 10);
+    const style = TextStyle(
+      fontSize: 10,
+      fontWeight: FontWeight.bold,
+      color: Color.fromARGB(255, 31, 52, 97),
+    );
 
     return SideTitleWidget(
       axisSide: meta.axisSide,
@@ -33,7 +101,7 @@ class LineChartView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<FlSpot>? spo = StatsGrid.spots;
+    List<FlSpot>? spo = spots;
 
     print(spo);
     return Card(
@@ -44,13 +112,14 @@ class LineChartView extends StatelessWidget {
         child: SingleChildScrollView(
             scrollDirection: Axis.horizontal,
             child: Container(
-              padding: EdgeInsets.only(bottom: 0, top: 15, right: 15, left: 15),
+              padding: EdgeInsets.only(bottom: 8, top: 15, right: 15, left: 15),
               height: 300,
-              width: MediaQuery.of(context).size.height * 1.5,
+              width: MediaQuery.of(context).size.height * 4,
               child: LineChart(
                 LineChartData(
-                  maxX: 9.5,
-                  maxY: StatsGrid.maxheat + 5,
+                  maxX: 49.5,
+                  minX: -0.5,
+                  maxY: maxvalue + 5,
                   lineTouchData: LineTouchData(enabled: true),
                   lineBarsData: [
                     LineChartBarData(
@@ -63,10 +132,11 @@ class LineChartView extends StatelessWidget {
                       ),
                     ),
                   ],
-                  minY: StatsGrid.minheat - 5,
+                  minY: minvalue - 5,
                   titlesData: FlTitlesData(
                     bottomTitles: AxisTitles(
                       sideTitles: SideTitles(
+                        reservedSize: 27,
                         showTitles: true,
                         interval: 1,
                         getTitlesWidget: bottomTitleWidgets,
@@ -76,8 +146,8 @@ class LineChartView extends StatelessWidget {
                       sideTitles: SideTitles(
                         showTitles: true,
                         getTitlesWidget: leftTitleWidgets,
-                        interval: 5,
-                        reservedSize: 30,
+                        interval: 3,
+                        reservedSize: 38,
                       ),
                     ),
                     topTitles: AxisTitles(
