cd6eb87f5e8bd35ebf69a83cef4c98ce14295bdc
Kutay Akgün <56596454+kutayakgn@users.noreply.github.com>
2022-08-18 19:11:13 +0300

    Add files via upload

---
+++ Mobile/sensor_mobile/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\kutay\\development\\sdks\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Projects\\SensorProject\\Mobile\\sensor_mobile" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\kutay\\development\\sdks\\flutter"
+  "PROJECT_DIR=C:\\Projects\\SensorProject\\Mobile\\sensor_mobile"
+  "FLUTTER_ROOT=C:\\Users\\kutay\\development\\sdks\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Projects\\SensorProject\\Mobile\\sensor_mobile\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Projects\\SensorProject\\Mobile\\sensor_mobile"
+  "FLUTTER_TARGET=C:\\Projects\\SensorProject\\Mobile\\sensor_mobile\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Projects\\SensorProject\\Mobile\\sensor_mobile\\.dart_tool\\package_config.json"
+)
